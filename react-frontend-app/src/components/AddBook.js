51445ddd7736334bb6b77359c85f1d8d5fe70e82
Jeff Peng <Jeff.Peng@capco.com>
2019-12-10 00:26:06 -0500

    Done adding data

--- react-frontend-app/src/components/AddBook.js
+++ react-frontend-app/src/components/AddBook.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import {gql} from 'apollo-boost'
-import { useQuery } from '@apollo/react-hooks';
-
-const getAuthorsQuery = gql`
-    {
-        authors{
-            name
-            id
-        }
-    }
-`
+import React, {useState} from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import {getAuthorsQuery, addBookMutation, getBooksQuery} from '../queries/querires'
 
 
 const AddBook = () => {
     const {loading, error, data} = useQuery(getAuthorsQuery)
+    const [name, setName] = useState("")
+    const [genre, setGenre] = useState("")
+    const [authorId, setAuthorId] = useState("")
+    const [addBookMutationFunc, { mutationStatus }] = useMutation(addBookMutation)
 
     const displayAuthors = () =>{
         if (loading) return <option>Loading Authors...</option>
@@ -26,21 +21,41 @@ const AddBook = () => {
         ))
     }
     
+    const submit = (e) =>{
+        e.preventDefault();
+        let selectedAuthor = authorId
+        if ( !error && data && data.authors && selectedAuthor === ""){
+            selectedAuthor = data.authors[0]['id']
+        }
+        let inputObj = {name, genre, authorId: selectedAuthor}
+        console.log(inputObj)
+        addBookMutationFunc({
+            variables: inputObj,
+            refetchQueries: [{
+                query: getBooksQuery // refresh the fetch books query in bookList
+            }]
+        })
+    }
+
+
     if (error) return <p>Error: can't connect</p>
 
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={submit} >
             <div className="field">
                 <label>Book Name:</label>
-                <input type="text" />
+                <input type="text" onChange={e=> setName(e.target.value)} />
             </div>
             <div className="field">
                 <label>Genre: </label>
-                <input type="text" />
+                <input type="text" onChange={e=> setGenre(e.target.value)} />
             </div>
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select onChange={e => {
+                    console.log(e.target.value)
+                    setAuthorId(e.target.value)
+                }} >
                     {displayAuthors()}
                 </select>
             </div>
---
+++ react-frontend-app/src/components/bookDetails.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+const BookDetails = () => {
+
+    return (
+        <div id="book-details">
+            <p>Book Details:</p>
+            
+        </div>
+    );
+}
+
+export default BookDetails;
